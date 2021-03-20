bdf3906abe616a8272830f60031c143e7528c30e
EmilieDeplanche <emilie.deplanche@hotmail.fr>
2021-03-20 19:36:18 +0100

    AppFixtures races and animals comments done

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,26 +12,29 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use App\DataFixtures\Provider\AnimalProvider;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+
+
 class AppFixtures extends Fixture
 {
-    private $passwordEncoder;
 
     private $connection;
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder, Connection $connection)
+    public function __construct(Connection $connection)
     {
-        $this->passwordEncoder = $passwordEncoder;
+        // Connection to MySQL
         $this->connection = $connection;
     }
 
+    // Here we set up our values
     const NB_ANIMALS = 30;
     const NB_SPECIES = 3;
     const NB_RACES = 9;
 
     private function truncate()
     {
+        // We desactivate constraints from Faker
         $animals = $this->connection->executeQuery('SET foreign_key_checks = 0');
-
+        // We truncate
         $animals = $this->connection->executeQuery('TRUNCATE TABLE animal');
         $animals = $this->connection->executeQuery('TRUNCATE TABLE race');
         $animals = $this->connection->executeQuery('TRUNCATE TABLE shelter');
@@ -39,81 +42,111 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-
+        // We truncate our tables to restart at id=1
         $this->truncate();
 
+        // Instance of Faker
         $faker = Faker\Factory::create('fr_FR');
+        // We give a provider to Faker
         $faker->addProvider(new AnimalProvider());
 
+
+        // Cat's Races for Race Table
+
+        // An array to stock our cat's races
         $catList = [];
 
         for ($i = 0; $i < self::NB_RACES; $i++) {
 
             $cats = new Race();
             $cats->setCreatedAt(new \DateTime());
+            // The function unique() make our result always unique
             $cats->setName($faker->unique()->catRace());
             $cats->setSpecies(1);
             
-            
+            // We stock our data for later
             $catList[] = $cats;
 
+            // We persist it
             $manager->persist($cats);
         };
 
+        // Dog's Races for Race Table
+
+        // An array to stock our dog's races
         $dogList = [];
 
         for ($i = 0; $i < self::NB_RACES; $i++) {
 
             $dogs = new Race();
-
+            // We fill the fields of our entity
             $dogs->setCreatedAt(new \DateTime());
             $dogs->setName($faker->unique()->dogRace());
             $dogs->setSpecies(2);
 
+            // We stock our data for later
             $dogList[] = $dogs;
 
+            // We persist it
             $manager->persist($dogs);
         };
 
+
+        // New pets's Races for Race Table
+
+        // An array to stock our new pet's races
         $nacList = [];
 
         for ($i = 0; $i < self::NB_RACES; $i++) {
 
             $nacs = new Race();
-
+            // We fill the fields of our entity
             $nacs->setCreatedAt(new \DateTime());
             $nacs->setName($faker->unique()->nacRace());
             $nacs->setSpecies(3);
 
+            // We stock our data for later
             $nacList[] = $nacs;
 
+            // We persist it
             $manager->persist($nacs);
         };
 
+        // Cats for Animal Table
+
         for ($i = 1; $i <= self::NB_ANIMALS; $i++) {
+
             $animal = new Animal();
-            $animal->setName($faker->firstname);
+            // We fill the fields of our entity
+            $animal->setName($faker->animalName());
             $animal->setBirthdate($faker->dateTimeBetween('-20 years'));
             $animal->setGender($faker->numberBetween($min = 1, $max = 2));
+            // The function numberBetween() allow to set a number only between the min and max values indicated
             $animal->setCohabitation($faker->numberBetween($min = 1, $max = 4));
             $animal->setPicture($faker->image());
             $animal->setStatus($faker->numberBetween($min = 1, $max = 4));
             $animal->setDescription($faker->text());
             $animal->setCreatedAt(new \DateTime());
 
+            // With shuffle we make random data but also unique
             shuffle($catList);
             for ($r = 0; $r < self::NB_RACES; $r++) {
-
+                // We search in our shuffled array
                 $randomRace = $catList[$r];
                 $animal->setRace($randomRace);
             }
 
+            // We persist it
             $manager->persist($animal);
         }
 
+        // Cats for Animal Table
+
         for ($i = 1; $i <= self::NB_ANIMALS; $i++) {
+
             $animal = new Animal();
-            $animal->setName($faker->firstname);
+            // We fill the fields of our entity
+            $animal->setName($faker->animalName());
             $animal->setBirthdate($faker->dateTimeBetween('-20 years'));
             $animal->setGender($faker->numberBetween($min = 1, $max = 2));
             $animal->setCohabitation($faker->numberBetween($min = 1, $max = 4));
@@ -122,6 +155,7 @@ class AppFixtures extends Fixture
             $animal->setDescription($faker->text());
             $animal->setCreatedAt(new \DateTime());
 
+            // With shuffle we make random data but also unique
             shuffle($dogList);
             for ($r = 0; $r < self::NB_RACES; $r++) {
 
@@ -129,12 +163,17 @@ class AppFixtures extends Fixture
                 $animal->setRace($randomRace);
             }
 
+            // We persist it
             $manager->persist($animal);
         }
 
+        // Cats for Animal Table
+
         for ($i = 1; $i <= self::NB_ANIMALS; $i++) {
+
             $animal = new Animal();
-            $animal->setName($faker->firstname);
+            // We fill the fields of our entity
+            $animal->setName($faker->animalName());
             $animal->setBirthdate($faker->dateTimeBetween('-20 years'));
             $animal->setGender($faker->numberBetween($min = 1, $max = 2));
             $animal->setCohabitation($faker->numberBetween($min = 1, $max = 4));
@@ -143,6 +182,7 @@ class AppFixtures extends Fixture
             $animal->setDescription($faker->text());
             $animal->setCreatedAt(new \DateTime());
 
+            // With shuffle we make random data but also unique
             shuffle($nacList);
             for ($r = 0; $r < self::NB_RACES; $r++) {
 
@@ -150,11 +190,12 @@ class AppFixtures extends Fixture
                 $animal->setRace($randomRace);
             }
 
+            // We persist it
             $manager->persist($animal);
         }
 
 
-
+        // We flush
         $manager->flush();
     }
 }
